5aa65299cf31a4f7044734821e26de2348cf47c1
Caleb Barnhart <barnhartcaleb16@gmail.com>
2019-10-15 15:01:07 -0400

    Asteroids collide

--- Outerboard/Assets/Scripts/Obstacle.cs
+++ Outerboard/Assets/Scripts/Obstacle.cs
@@ -3,37 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
-    private float xSpeed;
-    private float ySpeed;
-    private float xMax = 200f;
-    private float yMax = 200f;
+    private float xSpeed;                                                       //X speed
+    private float ySpeed;                                                       //Y speed
+    private float xMax = 200f;                                                  //X boundary
+    private float yMax = 200f;                                                  //Y boundary
+    private Vector3 deg;                                                        //Direction of motion
+
     // Start is called before the first frame update
     void Start() {
         xSpeed = Random.Range(-1f, 1f);
         ySpeed = Random.Range(-1f, 1f);
+        deg = new Vector3(xSpeed, ySpeed, 0f);
+
+        this.GetComponent<Rigidbody2D>().AddForce(deg * 1000);
     }
 
     // Update is called once per frame
     void Update() {
         Vector3 pos = transform.position;
-        pos.x += xSpeed;
-        pos.y += ySpeed;
 
-        if (pos.x >= xMax) {                                                            //Keeps the player in the game bounds
-            pos.x = xMax;
-            xSpeed = -xSpeed;
-        } else if (pos.x <= -xMax) {
-            pos.x = -xMax;
-            xSpeed = -xSpeed;
+        if (Mathf.Abs(pos.x) >= xMax) {
+            Destroy(this.gameObject);
         }
-        if (pos.y >= yMax) {
-            pos.y = yMax;
-            ySpeed = -ySpeed;
-        } else if (pos.y <= -yMax) {
-            pos.y = -yMax;
-            ySpeed = -ySpeed;
+        if (Mathf.Abs(pos.y) >= yMax) {
+            Destroy(this.gameObject);
         }
-
-        transform.position = pos;
     }
 }
